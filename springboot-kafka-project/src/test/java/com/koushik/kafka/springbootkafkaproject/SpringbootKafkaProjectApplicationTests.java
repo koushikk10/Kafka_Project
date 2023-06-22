c6c038a45f597f45760175ca0c769254307c156b
Your Name <karre.k@northeastern.edu>
2023-06-22 19:23:48 -0400

    added skeleton for service 1

---
+++ springboot-kafka-project/src/test/java/com/koushik/kafka/springbootkafkaproject/SpringbootKafkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koushik.kafka.springbootkafkaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
