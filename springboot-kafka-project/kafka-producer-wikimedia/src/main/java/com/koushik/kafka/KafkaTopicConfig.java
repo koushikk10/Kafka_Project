7d44d7894b08cea50b1a6cef3fb67f4175beca76
Your Name <karre.k@northeastern.edu>
2023-06-22 19:52:24 -0400

    create multimodule project, kafka producer, and topic

---
+++ springboot-kafka-project/kafka-producer-wikimedia/src/main/java/com/koushik/kafka/KafkaTopicConfig.java
@@ -0,0 +1,14 @@
+package com.koushik.kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+//Creating Kafka Topic
+@Configuration
+public class KafkaTopicConfig {
+    @Bean
+    public NewTopic topic(){
+        return TopicBuilder.name("wikimedia_recentchange").build();
+    }
+}
